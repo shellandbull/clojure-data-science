6c69146ec67854e600f1ce6874cfa19459b9d3a6
Mario G <mariojgintili@gmail.com>
2024-06-21 16:45:42 +0100

    add clojure chapter

---
+++ src/notebooks/clojure.clj
@@ -0,0 +1,76 @@
+(ns notebooks.clojure
+  (:require [tablecloth.api :as tc]
+            [scicloj.noj.v1.vis.hanami :as hanami]
+            [aerial.hanami.templates :as ht]
+            [scicloj.kindly.v4.kind :as kind]))
+;; # Clojure
+
+;; > Clojure is a dynamic, general-purpose programming language, combining the approachability and interactive development of a scripting language with an efficient and robust infrastructure for multithreaded programming.
+
+;; [source](https://clojure.org/)
+
+;; ## Literals
+
+;; ## Characters
+
+;; ## Symbols
+
+;; ## Collections
+
+;; ## Structure
+
+;; ## Delayed evaluation
+
+;; ## REPL, explorations & interactive programming
+
+;; ## Functions
+
+;; ### Multi arity functions
+
+;; ### Rest/split functions
+
+;; ### `defn` vs `fn`
+
+;; ## Applying functions
+
+;; ### `apply`
+
+;; ## Locals & closures
+
+;; ### `let`
+
+;; ### `defn`
+
+;; ### Closures
+
+;; ## Java Interoperability
+
+;; ## Sequential collections
+
+;; ### Vectors
+
+;; ### Lists
+
+;; ## Hashed collections
+
+;; ### Sets
+
+;; ### Maps
+
+;; ### Representing domain data
+
+;; ## Flow control
+
+;; ### Statements vs expressions
+
+;; ### Flow control expressions
+
+;; ## Recursion
+
+;; ## Exceptions
+
+;; ## Namespaces
+
+;; ### Declaring namespaces
+
+;; ### Java classes and imports
